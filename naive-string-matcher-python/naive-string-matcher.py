ef45030078bb2c4aec7fbd24b0d43f69e030bf76
RaphaelLight <raphael.light.77@gmail.com>
2023-11-28 08:21:51 +0800

    this is good for now I guess...

--- naive-string-matcher-python/naive-string-matcher.py
+++ naive-string-matcher-python/naive-string-matcher.py
@@ -1,14 +1,23 @@
 print('\nNaive String Matcher by Raphael Light')
 
-find_string = 'eye'
-search_string = 'eceyeye'
+while True:
+    find_string = input('\nEnter the string to find: ')
+    if find_string == '-999':
+        break
+    search_string = input('Enter the string to be searched: ')
+    if search_string == '-999':
+        break
 
-valid_shift = 0
-max_shift = len(search_string)-len(find_string)
+    if len(find_string) >= len(search_string):
+        print('\nFind string should be less than the search string.')
+        continue
 
-for start in range(0, max_shift+1):
-    end = start + len(find_string)
-    if find_string == search_string[start:end]:
-        valid_shift += 1
+    valid_shift = 0
+    max_shift = len(search_string)-len(find_string)
 
-print(valid_shift)
+    for start in range(0, max_shift+1):
+        end = start + len(find_string)
+        if find_string == search_string[start:end]:
+            valid_shift += 1
+
+    print(f'\nThe number of {find_string} in {search_string} is {valid_shift}')
